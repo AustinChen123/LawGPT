ba82f12338aac85ee2210c71c481381626d49095
Chun-Yi Chen <cge123321@gmail.com>
2026-01-08 23:09:24 +0100

    chore(infra): update pinecone index configuration
    Update target index to 'lawgpt-768' to match embedding model specifications.

--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,8 @@ class Settings:
     PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     CONFIG_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    DATA_FOLDER = os.path.join(CONFIG_FOLDER, "..\data\de")
-    INDEX_NAME = "lawgpt"
+    DATA_FOLDER = os.path.join(CONFIG_FOLDER, "../data/de")
+    INDEX_NAME = "lawgpt-768"
     ENDPOINT = "https://www.gesetze-im-internet.de"
     PINECONE_HOST = os.getenv("PINECONE_HOST")
-    TARGET_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-                   "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-                   "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    TARGET_LIST = ["A"]
